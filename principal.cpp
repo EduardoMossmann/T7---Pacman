9c4bad730694cc9ca0716ed17fabc2eaffac2b42
Luiz <luizflersch@gmail.com>
2018-12-07 23:31:58 -0200

    Update principal.cpp

--- principal.cpp
+++ principal.cpp
@@ -16,7 +16,7 @@ using namespace tela;
 using namespace geom;
 using namespace std;
 
-void cria_vertice(int matriz[19][21],int i, int j){
+void cria_vertice(char matriz[][21],int i, int j){
 
     if(i != 0){
         if(matriz[i-1][j] == '0'){
@@ -32,8 +32,21 @@ void cria_vertice(int matriz[19][21],int i, int j){
 
 void desenha_tela(Retangulo *r, Tela *t){
     while(r != nullptr){
-        t.retangulo(*r);
+        t->retangulo(*r);
+        r = r->prox;
     }
+    return;
+}
+
+void adiciona_retangulo(char matriz[][21], int i, int j,Retangulo ** lista){
+    Retangulo *r = new Retangulo;
+    r->pos.x = i*20;
+    r->pos.y = j*20;
+    r->prox = *lista;
+    r->tam.alt = 20;
+    r->tam.larg = 20;
+    *lista = r;
+    cout << "\n" << j;
     return;
 }
 
@@ -53,17 +66,19 @@ int main(int argc, char **argv) {
 
     for(int i = 0; i < 19; i++){
         getline(arq, mapa);
-        cout << mapa;
         for(int j = 0; j < 21; j++){
             matriz[i][j] = mapa[j];
             if(mapa[j] == 0)
                 cria_vertice(matriz,i,j);
-            if(mapa[j] == 1){
-                //listaretangulos
+            if(mapa[j] == '1'){
+                adiciona_retangulo(matriz,i,j,&lista);
             }
         }
-
     }
+    Cor azul = {0.2, 0.3, 0.8};
+    t.cor(azul);
+    desenha_tela(lista,&t);
+    system("pause");
 
   return 0;
 }
